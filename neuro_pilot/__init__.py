77697f73ed1cafb8b44c2f3dba0a7d0261c575c2
vtnguyen04 <thcs2nguyen@gmail.com>
2026-03-10 21:39:03 +0700

    chore: cleanup project root and finalize library-mode configuration

--- neuro_pilot/__init__.py
+++ neuro_pilot/__init__.py
@@ -1,7 +1,11 @@
-# NeuroPilot 🚀
-__version__ = "0.1.0"
+# NeuroPilot: Unified E2E Autonomous Driving Framework
+# Copyright (c) 2026 Vo Thanh Nguyen. All rights reserved.
+
+__version__ = "1.0.0"
 
 from neuro_pilot.engine.model import NeuroPilot
-from neuro_pilot.utils.logger import logger
+from neuro_pilot.engine.results import Results
+from neuro_pilot.utils.checks import check_version
 
-__all__ = ("__version__", "NeuroPilot", "logger")
+# Export common classes for easy access: from neuro_pilot import NeuroPilot
+__all__ = ["NeuroPilot", "Results", "check_version"]
